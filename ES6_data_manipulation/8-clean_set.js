12fa4f492fd9cd9cf27ba7dccff71a52ea01efd7
Lachlan <lachlan.luchetti@outlook.com>
2026-08-30 14:17:15 +1000

    ES6_data_manipulation/

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+export default function cleanSet(set, startString) {
+  if (!startString) {
+    return '';
+  }
+
+  const result = [];
+
+  set.forEach((value) => {
+    if (value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  });
+
+  return result.join('-');
+}
