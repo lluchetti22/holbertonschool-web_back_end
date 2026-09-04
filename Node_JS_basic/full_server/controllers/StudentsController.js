f1d3ada4ad9a32223d7ecbef0138227af8bc0aa1
Lachlan <lachlan.luchetti@outlook.com>
2026-09-04 15:25:13 +1000

    Node_JS_basic/

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,7 +1,47 @@
-class AppController {
-  static getHomepage(request, response) {
-    response.status(200).send('Hello Holberton School!');
+import readDatabase from '../utils';
+
+class StudentsController {
+  static getAllStudents(request, response) {
+    const dbPath = process.argv[2];
+
+    readDatabase(dbPath)
+      .then((fields) => {
+        const responseText = ['This is the list of our students'];
+        const sortedFields = Object.keys(fields).sort((a, b) =>
+          a.localeCompare(b, undefined, { sensitivity: 'base' })
+        );
+
+        for (const field of sortedFields) {
+          const count = fields[field].length;
+          const list = fields[field].join(', ');
+          responseText.push(`Number of students in ${field}: ${count}. List: ${list}`);
+        }
+
+        return response.status(200).send(responseText.join('\n'));
+      })
+      .catch(() => {
+        return response.status(500).send('Cannot load the database');
+      });
+  }
+
+  static getAllStudentsByMajor(request, response) {
+    const { major } = request.params;
+
+    if (major !== 'CS' && major !== 'SWE') {
+      return response.status(500).send('Major parameter must be CS or SWE');
+    }
+
+    const dbPath = process.argv[2];
+
+    readDatabase(dbPath)
+      .then((fields) => {
+        const list = fields[major] ? fields[major].join(', ') : '';
+        return response.status(200).send(`List: ${list}`);
+      })
+      .catch(() => {
+        return response.status(500).send('Cannot load the database');
+      });
   }
 }
 
-export default AppController;
+export default StudentsController;
--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,29 +1,28 @@
 import fs from 'fs';
 
-const readDatabase = (path) => new Promise((resolve, reject) => {
-  fs.readFile(path, 'utf-8', (err, fileContent) => {
-    if (err) {
-      reject(err);
-      return;
-    }
-
-    const lines = fileContent
-      .split('\n')
-      .filter((line) => line.trim() !== '');
-
-    const students = lines.slice(1);
-    const fields = {};
-
-    students.forEach((line) => {
-      const [firstname, , , field] = line.split(',');
-      if (!fields[field]) {
-        fields[field] = [];
+const readDatabase = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+      if (err) {
+        reject(err);
+        return;
       }
-      fields[field].push(firstname);
-    });
+      const lines = data.trim().split('\n');
+      const students = lines.slice(1).filter((line) => line.trim() !== '');
+      const fields = {};
 
-    resolve(fields);
+      for (const student of students) {
+        const [firstname, , , field] = student.split(',');
+        if (firstname && field) {
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+          fields[field].push(firstname);
+        }
+      }
+      resolve(fields);
+    });
   });
-});
+};
 
 export default readDatabase;
